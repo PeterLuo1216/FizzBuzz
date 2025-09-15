6e3def5027667f50c1c9eb297aeb8c3b87e90e5a
peterluo <realpeter.luo@mail.utoronto.ca>
2025-09-15 03:17:49 -0400

    Everything Done

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,14 +2,19 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        int count = multiples(1000, 3 ,5);
+        System.out.println(count);
+
+    }
+
+    static int multiples(int n, int a, int b) {
         int count = 0;
-        for (int i = 1; i <= 1000; i++) {
+        for (int i = 1; i < 1000; i++) {
             if (i % 3 ==0 || i % 5 ==0) {
                 count ++;
             }
 
         }
-        System.out.println(count);
-
+        return count;
     }
 }
